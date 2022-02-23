8e265c48e6da691e72ae7adbd21bc45a1feb67a5
James Robertson <digital.robertson@gmail.com>
2022-02-23 10:48:41 +0000

    minor improvement: Using the new rxfreader gem.

--- lib/spstrigger_execute.rb
+++ lib/spstrigger_execute.rb
@@ -5,49 +5,51 @@
 require 'dynarex'
 require 'chronic_between'
 require 'xmlregistry_objects'
+require 'rxfreader'
 
 
 class SPSTriggerExecute
 
   def initialize(x, reg=nil, polyrexdoc=nil, rws=nil, log: nil)
-    
+
     log.info 'SPSTriggerExecute/initialize: active' if log
-    
+
     @rws, @log = rws, log
-    
+
     @patterns = if x.is_a? Dynarex then
-    
+
       x.to_h
-      
+
     elsif x.is_a? Array
       x
     else
-      
-      buffer, _ = RXFHelper.read x, auto: false
+
+      buffer, _ = RXFReader.read x
       dx = buffer[/^<\?dynarex /] ? Dynarex.new.import(buffer) : \
           Dynarex.new(buffer)
       dx.to_a
-      
-    end    
-    
+
+    end
+
     if reg and polyrexdoc then
-      log.info 'SPSTriggerExecute/initialize: before reg' if log      
+      log.info 'SPSTriggerExecute/initialize: before reg' if log
       puts 'reg: ' + reg.inspect
       xro = XMLRegistryObjects.new(reg, polyrexdoc, log: log)
-      log.info 'SPSTriggerExecute/initialize: after reg' if log      
-      @h = xro.to_h      
-      define_methods = @h.keys.map {|x| "def #{x}() @h[:#{x}] end"}      
-      instance_eval define_methods.join("\n")      
-      
+      log.info 'SPSTriggerExecute/initialize: after reg' if log
+      @h = xro.to_h
+      define_methods = @h.keys.map {|x| "def #{x}() @h[:#{x}] end"}
+      instance_eval define_methods.join("\n")
+
       if log then
-        log.info 'SPSTriggerExecute/initialize: define_methods : ' + 
+        log.info 'SPSTriggerExecute/initialize: define_methods : ' +
             self.public_methods.sort.inspect
       end
     end
 
   end
 
-  def match_and_execute(topic: nil, message: message)
+  def match_and_execute(topicx=nil, messagex=nil, topic: topicx,
+                        message: messagex)
 
     @log.info 'SPSTriggerExecute/match_and_execute: active' if @log
     results = find_match topic, message
@@ -55,31 +57,31 @@ class SPSTriggerExecute
   end
 
   alias mae match_and_execute
-  
+
   def run(s)
     instance_eval s
   end
-    
+
 
   private
 
   def find_match(topicx=nil, message)
-    
-    
+
+
     @patterns.map.with_index.inject([]) do |r, row|
 
       h, i = row
 
       topic, msg,  conditions, job = \
                       %i(topic msg conditions job).map {|x| h[x].to_s }
-      
+
       index ||= i + 1
-      
+
       # note: the index is only present if there is a duplicate
       #       Dynarex record default key
 
       t, m = topic.length > 0, msg.length > 0
-      
+
       result = if topicx && t && m then
 
         r1 = topicx.match(/#{topic}/)
@@ -94,98 +96,98 @@ class SPSTriggerExecute
 
       elsif m then
 
-        r2 = message.match(/#{msg}/)      
+        r2 = message.match(/#{msg}/)
         {msg: r2, index: index} if r2
       else
         {}
-      end      
+      end
 
       if result and conditions.length > 0 then
-        
+
         if @log then
-          @log.info 'SPSTriggerExecute/find_match: conditions: ' + 
+          @log.info 'SPSTriggerExecute/find_match: conditions: ' +
               conditions.inspect
         end
 
         named_match = message.match(/#{msg}/)
-        
+
         variable_assignment = if named_match then
-                  
+
           named_match.names.inject('') do |rs, name|
 
             m = msg =~ /\?<#{name}\>\\d+/ ? 'to_i' : 'to_s'
-            rs << "#{name} = named_match[:#{name}].#{m}\n"            
+            rs << "#{name} = named_match[:#{name}].#{m}\n"
           end
-          
-        else ''  
+
+        else ''
         end
 
 
         success = eval (variable_assignment + conditions)
 
         if @log then
-          @log.info 'SPSTriggerExecute/find_match: success : '  + 
+          @log.info 'SPSTriggerExecute/find_match: success : '  +
               success.inspect
         end
-        
+
         result = nil unless success
       end
 
       result ? r << result : r
-    
+
     end
   end
 
-  
+
   # not yet implemented
-=begin  
+=begin
   def method_missing(method_name, *args)
 
     job = args.shift
     # Rsc object call goes here
     @log.debug 'package: ' + package.inspect
     @rws.run_job package=method_name, job, {}, args
-  end  
-=end  
-  
+  end
+=end
+
   def prepare_jobs(results)
-    
+
     @log.info 'SPSTriggerExecute/prepare_jobs: active' if @log
 
     results.inject([]) do |r,h|
-      
+
       if @log then
-        @log.info 'SPSTriggerExecute/prepare_jobs: inside inject h: ' + 
-            h.inspect 
+        @log.info 'SPSTriggerExecute/prepare_jobs: inside inject h: ' +
+            h.inspect
       end
 
       a = []
       a += h[:topic].captures if h[:topic] && h[:topic].captures.any?
       a += h[:msg].captures if h[:msg]
-      
+
       params = {}
       params.merge!(h[:topic].named_captures) if h[:topic]
-      params.merge!(h[:msg].named_captures) if h[:msg] 
-      
+      params.merge!(h[:msg].named_captures) if h[:msg]
+
       jobs = @patterns[h[:index].to_i - 1][:job]
-      
-      
+
+
       jobs.split(/\s*;\s*/).each do |job|
 
         job_args = job.split + a
-        
+
         if job[/^\/\//] then
 
           r << [:rse, job_args, params]
 
-          
+
         elsif job[/^rse:\/\//]
-          
-          r << [:rse_uri, job]          
-          
+
+          r << [:rse_uri, job]
+
         elsif job[/^[$\w\/]+:/]
 
-          topic_message = job.gsub(/\$\d/) do |x| 
+          topic_message = job.gsub(/\$\d/) do |x|
 
             i = x[/\d$/].to_i - 1
             x.sub(/\$\d/, a[i].to_s)
@@ -193,33 +195,33 @@ class SPSTriggerExecute
 
           topic_message = topic_message\
             .gsub(/![Tt]ime/,Time.now.strftime("%a %H:%M%P"))\
-            .gsub(/![Dd]ate/,Time.now.strftime("%d %b"))        
+            .gsub(/![Dd]ate/,Time.now.strftime("%d %b"))
 
-          
-          r << [:sps, topic_message]          
+
+          r << [:sps, topic_message]
         else
-          
-          statement = job.gsub(/\$\d/) do |x| 
+
+          statement = job.gsub(/\$\d/) do |x|
             i = x[/\d$/].to_i - 1
             x.sub(/\$\d/, a[i].to_s)
           end
-          
+
           r << [:ste, statement]
-          
+
         end
       end
-      
+
       @log.info 'SPSTriggerExecute/prepare_jobs: result: '  if @log
-      
-      r  
-      
+
+      r
+
     end
   end
 
   def time()
 
     t = Time.now
-    
+
     def t.within?(times)
       ChronicBetween.new(times).within? Time.now
     end
@@ -230,9 +232,9 @@ class SPSTriggerExecute
 
     def t.<(x)
       x.is_a?(String) ? self < Chronic.parse(x) : super(x)
-    end    
+    end
 
     t
   end
-    
+
 end
--- spstrigger_execute.gemspec
+++ spstrigger_execute.gemspec
@@ -1,16 +1,16 @@
 Gem::Specification.new do |s|
   s.name = 'spstrigger_execute'
-  s.version = '0.5.1'
+  s.version = '0.5.2'
   s.summary = 'An SPS client built for reponding to messages which match ' + 
       'keywords and conditions.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/spstrigger_execute.rb']
-  s.add_runtime_dependency('dynarex', '~> 1.8', '>=1.8.25')
-  s.add_runtime_dependency('chronic_between', '~> 0.3', '>=0.3.1')
-  s.add_runtime_dependency('xmlregistry_objects', '~> 0.7', '>=0.7.5')
+  s.add_runtime_dependency('dynarex', '~> 1.9', '>=1.9.6')
+  s.add_runtime_dependency('chronic_between', '~> 0.5', '>=0.5.0')
+  s.add_runtime_dependency('xmlregistry_objects', '~> 0.8', '>=0.8.0')
   s.signing_key = '../privatekeys/spstrigger_execute.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/spstrigger_execute'
 end
